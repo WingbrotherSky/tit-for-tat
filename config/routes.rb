ab39140894263c3111a2f1c510f560dd0486e621
Jacob Tompkins <term171013@gmail.com>
2018-05-14 19:38:41 +0800

    completed login controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # API Namespace
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      post '/login', to: 'login#login'
+    end
+  end
 end

99a47a9c33f2e763c47178e4d69280067c67a49e
Ashton Burchfield <underathousandstars@outlook.com>
2018-05-15 14:47:41 +0800

    fixed errors in merged controllers and models with api

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -1,30 +1,27 @@
 class Api::V1::LoginController < Api::V1::BaseController
-  URL = "https://api.weixin.qql.com/sns/jscode2session".freeze
+  URL = "https://api.weixin.qq.com/sns/jscode2session".freeze
 
   def login
-    p params
-    @user = User.find_or_create_by(open_id: wechat_user.fetch('openid'))
-  
-  render json: {
-    userId: @user.id
-  }
+    @user = User.find_or_create_by(openid: wechat_user.fetch('openid'))
+    render json: {
+      userId: @user.id
+    }
   end
 
   private
 
   def wechat_params
     {
-      appId: ENV['appId'],
-      appSecret: ENV['appSecret'],
+      appid: ENV['appId'],
+      secret: ENV['appSecret'],
       js_code: params[:code],
       grant_type: 'authorization_code'
     }
   end
 
   def wechat_user
+    p 'calling tencent'
     @wechat_response ||= RestClient.post(URL, wechat_params)
     @wechat_user ||= JSON.parse(@wechat_response.body)
-    p @wechat_response
-    p @wechat_user
   end
 end

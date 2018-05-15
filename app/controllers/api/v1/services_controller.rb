d2553db998e582a51ae8bf8d5e5fc6bc5dc2428e
Jacob Tompkins <term171013@gmail.com>
2018-05-15 12:04:58 +0800

    finished first draft of basic CRUD

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -9,15 +9,36 @@ class Api::V1::ServicesController < Api::V1::BaseController
   end
 
   def create
-
+    @service = Service.new(service_params)
+    if @service.save
+      render json: { message: "Service created" }
+    else
+      render json: { errors: @service.errors.messages }
+    end
   end
 
   def update
-
+    if Service.update(service_params)
+      render json: { message: "Service updated" }
+    else
+      render json: { errors: "Service could not be updated" }
+    end
   end
 
   def destroy
-
+    @service = Service.find(params[:id])
+    if @service.destroy
+      render json: { message: "Service destroyed" }
+    else
+      render json: { errors: @json.errors.messages }
+    end
   end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:name, :user_id, :description, :image)
+  end
+
 
 end

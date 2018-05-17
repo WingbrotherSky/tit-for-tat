50ac02b14e59c4c26bbf649c9d540455911f7e22
Jacob Tompkins <term171013@gmail.com>
2018-05-17 18:59:07 +0800

    fixed strong params for user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,6 +14,6 @@ class Api::V1::UsersController < Api::V1::BaseController
   private
 
   def user_params
-    params.require(:user).permit(:openid, :username, :bio)
+    params.require(:user).permit(:openid, :username, :bio, :location, :profile_image)
   end
 end

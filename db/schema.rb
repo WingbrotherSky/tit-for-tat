1df3ab74e385b474f5ac872600553bc3d1c5f495
Jacob Tompkins <term171013@gmail.com>
2018-05-17 18:57:38 +0800

    Added location and profile image to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180515062149) do
+ActiveRecord::Schema.define(version: 20180517105639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 20180515062149) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.text "bio"
+    t.string "location"
+    t.string "profile_image"
   end
 
   add_foreign_key "contracts", "services"

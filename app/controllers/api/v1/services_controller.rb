b8b9f83d28263bc744c7c7706b0a32d5fd7621bf
Jacob Tompkins <term171013@gmail.com>
2018-05-18 15:25:19 +0800

    added validation to services create

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -10,10 +10,14 @@ class Api::V1::ServicesController < Api::V1::BaseController
 
   def create
     @service = Service.new(service_params)
-    if @service.save
-      render json: { userId: @service.user.id }
+    unless service.user.services.count == 3
+      if @service.save
+        render json: { userId: @service.user.id }
+      else
+        error_message(@service)
+      end
     else
-      error_message(@service)
+      render json: { errors: "User cannot have more than 3 services"}
     end
   end
 
